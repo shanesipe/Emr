eb14fe85164c2e53e0766065ae9043f253fe4d2a
shanesipe <shanesipe17@gmail.com>
2023-07-11 17:56:32 -0500

    Update patient.go

--- services/patient.go
+++ services/patient.go
@@ -1,32 +1,13 @@
-package models
-
-import "time"
-
-// Patient represents a patient record
-type Patient struct {
-  ID        string     `json:"id"`
-  Name      string     `json:"name"` 
-  DateOfBirth time.Time `json:"dateOfBirth"`
-  Gender    string     `json:"gender"`
-  Address   string     `json:"address"`
-  Phone     string     `json:"phone"`
-  Email     string     `json:"email"`
+type PatientService interface {
+  GetPatient(id string) (*Patient, error) 
+  CreatePatient(p *Patient) error
+  // other methods  
 }
 
-// TableName sets Patient's table name to be `patients`
-func (Patient) TableName() string {
-  return "patients" 
+type DefaultPatientService struct {
+  repo PatientRepository
 }
 
-// PatientSummary is a summarized view of a patient
-type PatientSummary struct {
-  ID        string     `json:"id"`
-  Name      string     `json:"name"`
-  DateOfBirth time.Time `json:"dateOfBirth"`
-  Gender    string     `json:"gender"` 
-}
-
-// TableName sets PatientSummary's table name to be `patient_summaries`
-func (PatientSummary) TableName() string {
-  return "patient_summaries"
-}
+func (s *DefaultPatientService) GetPatient(id string) (*Patient, error) {
+  return s.repo.GetPatient(id)
+} 
