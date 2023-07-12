09cdf9860212da12451271a8d59a202725f6a674
shanesipe <shanesipe17@gmail.com>
2023-07-12 10:01:48 -0500

    Update appointment.go

--- models/appointment.go
+++ models/appointment.go
@@ -1,7 +1,58 @@
+package models
+
+import (
+    "time"
+
+    "github.com/google/uuid"
+)
+
+// Appointment represents a doctor appointment booked by a patient
 type Appointment struct {
-  ID string
-  PatientID string
-  DoctorID string
-  Date time.Time
-  Notes string
+    ID        string    `json:"id"`
+    PatientID string    `json:"patient_id"`
+    DoctorID  string    `json:"doctor_id"`
+    Date      time.Time `json:"date"`
+    Notes     string    `json:"notes"`
 }
+
+// NewAppointment creates a new appointment
+func NewAppointment(patientID, doctorID string, date time.Time, notes string) *Appointment {
+    return &Appointment{
+        ID:        uuid.New().String(),
+        PatientID: patientID,
+        DoctorID:  doctorID,
+        Date:      date,
+        Notes:     notes,
+    }
+}
+
+// IsValid validates the appointment
+func (a *Appointment) IsValid() bool {
+    return a.PatientID != "" && a.DoctorID != "" && !a.Date.IsZero()
+}
+
+// GetPatientID returns the patient ID
+func (a *Appointment) GetPatientID() string {
+    return a.PatientID
+}
+
+// GetDoctorID returns the doctor ID  
+func (a *Appointment) GetDoctorID() string {
+    return a.DoctorID
+}
+
+// GetDate returns the appointment date
+func (a *Appointment) GetDate() time.Time {
+    return a.Date
+}
+
+// GetNotes returns the appointment notes
+func (a *Appointment) GetNotes() string {
+    return a.Notes
+}
+
+// SetNotes sets the appointment notes
+func (a *Appointment) SetNotes(notes string) {
+    a.Notes = notes
+}
+
