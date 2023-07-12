7de13d7c2f0080a4aad692afe158d6ad2a7b131d
shanesipe <shanesipe17@gmail.com>
2023-07-12 10:09:26 -0500

    Update lab.go

--- models/lab.go
+++ models/lab.go
@@ -1,12 +1,60 @@
-type LabOrder struct {
+package models
+
+import "time"
+
+// Lab represents a medical lab facility
+type Lab struct {
   ID string
-  PatientID string
-  TestCodes []string
-  DateOrdered time.Time
+  Name string
+  Address string
 }
 
-type LabResult struct {
+// NewLab creates a new Lab 
+func NewLab(name, address string) *Lab {
+  return &Lab{
+    ID: uuid.New().String(),
+    Name: name,
+    Address: address,
+  }
+}
+
+// GetID returns the lab ID
+func (l *Lab) GetID() string {
+  return l.ID
+} 
+
+// GetName returns the lab name
+func (l *Lab) GetName() string {
+  return l.Name
+}
+
+// GetAddress returns the lab address
+func (l *Lab) GetAddress() string {
+  return l.Address
+}
+
+// SetAddress updates the lab address
+func (l *Lab) SetAddress(address string) {
+  l.Address = address
+}
+
+// LabTest represents a medical test conducted by a lab
+type LabTest struct {
   ID string
-  LabOrderID string
-  Results map[string]interface{}
+  LabID string
+  PatientID string
+  TestType string
+  Date time.Time
+  Result string
+}
+
+// NewLabTest creates a new lab test
+func NewLabTest(labID, patientID, testType string) *LabTest {
+  return &LabTest{
+    ID: uuid.New().String(),
+    LabID: labID,
+    PatientID: patientID,
+    TestType: testType,
+    Date: time.Now(),
+  }
 }
