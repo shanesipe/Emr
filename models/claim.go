496d9c4900fc7a5cf4366c5315366f31ac268b91
shanesipe <shanesipe17@gmail.com>
2023-07-12 10:06:12 -0500

    Update claim.go

--- models/claim.go
+++ models/claim.go
@@ -1,12 +1,60 @@
+package models
+
+import "time"
+
+// Claim represents an insurance claim
 type Claim struct {
-  ID string
-  PatientID string
-  TotalCost float64
-  LineItems []ClaimLineItem
+    ID string
+    PatientID string
+    AppointmentID string
+    Date time.Time
+    Amount float64
+    Status string
 }
 
-type ClaimLineItem struct {
-  ID string
-  Code string
-  Cost float64
+// NewClaim creates a new claim
+func NewClaim(patientID, appointmentID string, amount float64) *Claim {
+    return &Claim{
+        ID: uuid.New().String(),
+        PatientID: patientID,
+        AppointmentID: appointmentID,
+        Date: time.Now(),
+        Amount: amount,
+        Status: "Pending",
+    }
+}
+
+// GetID returns the claim ID
+func (c *Claim) GetID() string {
+    return c.ID
+}
+
+// GetPatientID returns the patient ID
+func (c *Claim) GetPatientID() string {
+    return c.PatientID
+}
+
+// GetAppointmentID returns the appointment ID
+func (c *Claim) GetAppointmentID() string {
+    return c.AppointmentID
+}
+
+// GetDate returns the date of the claim
+func (c *Claim) GetDate() time.Time {
+    return c.Date
+}
+
+// GetAmount returns the claim amount
+func (c *Claim) GetAmount() float64 {
+    return c.Amount
+}
+
+// GetStatus returns the status of the claim
+func (c *Claim) GetStatus() string {
+    return c.Status
+}
+
+// SetStatus sets the status of the claim
+func (c *Claim) SetStatus(status string) {
+    c.Status = status
 }
