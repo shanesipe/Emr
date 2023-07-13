af80209f08371853d9a790c22fca626c5fa49c0a
shanesipe <shanesipe17@gmail.com>
2023-07-13 12:59:45 -0500

    Update appointment.go

--- services/appointment.go
+++ services/appointment.go
@@ -1,7 +1,23 @@
-type AppointmentService interface {
-  GetAppointment(id string) (*Appointment, error)
-  CreateAppointment(a *Appointment) error
-  // other methods
+package models
+
+import "time"
+
+type Appointment struct {
+  ID string
+  PatientID string
+  DoctorID string 
+  Date time.Time
+  Notes string
 }
 
-// Implement service
+func NewAppointment(patientID, doctorID string, date time.Time, notes string) *Appointment {
+  return &Appointment{
+    ID:       // generate ID
+    PatientID: patientID,
+    DoctorID: doctorID,
+    Date: date,
+    Notes: notes, 
+  }
+}
+
+// Add other methods like GetID(), GetDate() etc
