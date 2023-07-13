bda706badc657e239eda6634f80a40b75ceda17c
shanesipe <shanesipe17@gmail.com>
2023-07-13 13:05:26 -0500

    Update appointment.go

--- services/appointment.go
+++ services/appointment.go
@@ -1,23 +1,64 @@
-package models
+package services
 
-import "time"
+import (
+    "context"
+    "errors"
 
-type Appointment struct {
-  ID string
-  PatientID string
-  DoctorID string 
-  Date time.Time
-  Notes string
+    "emr/models"
+    "emr/repositories"
+)
+
+// AppointmentService provides appointment management functions
+type AppointmentService interface {
+    GetAppointment(ctx context.Context, id string) (*models.Appointment, error)
+    CreateAppointment(ctx context.Context, appointment *models.Appointment) error
+    UpdateAppointment(ctx context.Context, appointment *models.Appointment) error
+    DeleteAppointment(ctx context.Context, id string) error
 }
 
-func NewAppointment(patientID, doctorID string, date time.Time, notes string) *Appointment {
-  return &Appointment{
-    ID:       // generate ID
-    PatientID: patientID,
-    DoctorID: doctorID,
-    Date: date,
-    Notes: notes, 
-  }
+type appointmentService struct {
+    appointmentRepo repositories.AppointmentRepository
 }
 
-// Add other methods like GetID(), GetDate() etc
+// GetAppointment retrieves an appointment by ID
+func (s *appointmentService) GetAppointment(ctx context.Context, id string) (*models.Appointment, error) {
+    appointment, err := s.appointmentRepo.Get(ctx, id)
+    if err != nil {
+        return nil, err
+    }
+    return appointment, nil
+}
+
+// CreateAppointment saves a new appointment
+func (s *appointmentService) CreateAppointment(ctx context.Context, appointment *models.Appointment) error {
+    err := s.appointmentRepo.Save(ctx, appointment)
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
+// UpdateAppointment updates an existing appointment
+func (s *appointmentService) UpdateAppointment(ctx context.Context, appointment *models.Appointment) error {
+    // Validate appointment exists
+    if appointment.ID == "" {
+        return errors.New("invalid appointment")
+    }
+    
+    return s.appointmentRepo.Update(ctx, appointment)
+}
+
+// DeleteAppointment removes an appointment
+func (s *appointmentService) DeleteAppointment(ctx context.Context, id string) error {
+    // Validate ID
+    if id == "" {
+        return errors.New("invalid ID")
+    }
+    
+    return s.appointmentRepo.Delete(ctx, id)
+}
+
+// NewAppointmentService creates an AppointmentService
+func NewAppointmentService(appointmentRepo repositories.AppointmentRepository) AppointmentService {
+    return &appointmentService{appointmentRepo: appointmentRepo}
+}
