5a24673d56eb7a9d58061e1ac82ad7a636983c0c
shanesipe <shanesipe17@gmail.com>
2023-07-13 13:03:09 -0500

    Update billing.go

--- services/billing.go
+++ services/billing.go
@@ -1,6 +1,50 @@
+package services
+
+import (
+  "context"
+
+  "emr/models"
+  "emr/repositories"
+)
+
+// BillingService generates invoices for appointments
 type BillingService interface {
-  SubmitClaim(claim *Claim) error
-  GetClaimStatus(id string) (*ClaimStatus, error) 
+  GenerateInvoice(ctx context.Context, appointmentID string) (*models.Invoice, error)
 }
 
-// Implement service
+type billingService struct {
+  appointmentRepo repositories.AppointmentRepository
+}
+
+// GenerateInvoice creates an invoice for an appointment
+func (b *billingService) GenerateInvoice(ctx context.Context, appointmentID string) (*models.Invoice, error) {
+  // Get appointment
+  appointment, err := b.appointmentRepo.GetAppointment(ctx, appointmentID)
+  if err != nil {
+    return nil, err
+  }
+
+  // Create invoice
+  invoice := models.NewInvoice(appointment.PatientID, appointment.DoctorID, appointment.Date)
+
+  // Calculate amount
+  // Apply discounts
+
+  // Save invoice
+  err = b.invoiceRepo.Save(ctx, invoice)
+  if err != nil {
+    return nil, err 
+  }
+
+  return invoice, nil
+}
+
+// NewBillingService creates a BillingService 
+func NewBillingService(appointmentRepo repositories.AppointmentRepository, 
+                       invoiceRepo repositories.InvoiceRepository) BillingService {
+  return &billingService{
+    appointmentRepo: appointmentRepo,
+    invoiceRepo: invoiceRepo,
+  }
+}
+
